ea6e44801d414f282969a91adb89dbd24ae5117e
Félix Dorn <github@felixdorn.fr>
2021-01-23 09:29:47 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -6,28 +6,21 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Browsershot\Browsershot;
 
 Route::get('/_/banners/generate', function (Request $request) {
-    $request->validate([
-        'title' => 'required|string',
-        'body' => 'required|string',
-        'image' => 'required|string',
-        'width' => 'required|numeric',
-        'height' => 'required|numeric'
-    ]);
-
     return Generator::make(
-        $request->query('title'),
-        $request->query('body'),
-        $request->query('image'),
-        $request->query('theme'),
-        $request->query('width'),
-        $request->query('height')
+        $request->query('title') ?? config('banners.title'),
+        $request->query('body') ?? config('banners.body'),
+        $request->query('image') ?? config('banners.image'),
+        $request->query('theme') ?? config('banners.theme'),
+        $request->query('width') ?? config('banners.width'),
+        $request->query('height') ?? config('banners.height')
     );
 })->name('generate-banner');
 
 
 Route::get('/_/banners/render', function (Request $request) {
     $request->validate(['payload' => 'required|json']);
-    $data = json_decode($request->payload, true, 512, JSON_THROW_ON_ERROR);
+    $data = json_decode($request->payload, true, 512);
+    $data = array_merge(config('banners'), $data);
 
     return view('banners::render', $data);
 })->name('render-banner');
